aacd5669f092b950349452e005669f4a8e267301
AlejoGarat <alejogaratd@gmail.com>
2023-11-06 19:34:17 -0300

    Working on pythautomata conversion

--- automata_generator.py
+++ automata_generator.py
@@ -1,8 +1,14 @@
+import numpy as np
 from automata import Automata
 from exporter import export_automatas
-import networkx as nx
+from pythautomata.automata.deterministic_finite_automaton import DeterministicFiniteAutomaton as DFA
+from pythautomata.base_types.state import State
+from pythautomata.base_types.symbol import Symbol
+from pythautomata.base_types.alphabet import Alphabet
+from pythautomata.utilities.nicaud_dfa_generator import generate_dfa
+from pymodelextractor.learners.observation_table_learners.general_lstar_learner import GeneralLearner
 
-def generate_and_export_automatas(property: int, n: int, path: str):
+def generate_and_export_automatas(property: str, amount: int, nominal_size: int, alphabet_size: int, path: str):
     """Generates n automatas from a given property and exports them to a pickle file.
     
     Args:
@@ -10,11 +16,20 @@ def generate_and_export_automatas(property: int, n: int, path: str):
         n (int): The number of automatas to generate.
         path (str): The path to the pickle file.
     """
-    automatas = generate_many_automatas(property, n)
+    automatas = generate_many_automatas(amount, nominal_size, alphabet_size)
+    if property == "connected":
+        automatas = [pythautomata_automata_to_automata(make_automata_connected(automata)) for automata in automatas]
+    elif property == "fully_accepting":
+        automatas = [pythautomata_automata_to_automata(make_automata_unique_accepting(automata)) for automata in automatas]
+    elif property == "minimal":
+        automatas = [pythautomata_automata_to_automata(make_automata_minimal(automata)) for automata in automatas]
+    else:
+        raise ValueError("Unknown property")
+
     export_automatas(automatas, path)
 
-def generate_many_automatas(property: int, n: int) -> [Automata]:
-    """Generates n automatas from a given property.
+def generate_many_automatas(amount: int, nominal_size: int, alphabet_size: int) -> [DFA]:
+    """Generates n automatas using Nicaud.
     
     Args:
         property (int): The property to generate the automatas from.
@@ -23,64 +38,62 @@ def generate_many_automatas(property: int, n: int) -> [Automata]:
     Returns:
         [Automata]: The generated automatas.
     """
+    alphabet = lambda: Alphabet([Symbol(str(i)) for i in range(alphabet_size)])
     generated_automatas = []
-    for _ in range(n):
-        generated_automatas.append(generate_automata(property))
+    for _ in range(amount):
+        generated_automatas = generate_dfa(alphabet=alphabet, nominal_size=nominal_size)
     
     return generated_automatas
-    
-def generate_automata(property: int) -> Automata:
-    """Generates an automata from a given property.
 
+def pythautomata_automata_to_automata(dfa: DFA) -> Automata:
+    """Converts a pythautomata DFA to an Automata.
+    
     Args:
-        property (int): The property to generate the automata from.
-
-    Returns:
-        Automata: The generated automata.
-    """
-    if property == "connected":
-        return generate_connected_automata()
-    elif property == "fully_accepting":
-        return generate_fully_accepting_automata()
-    elif property == "minimal":
-        return generate_minimal_automata()
-    
-    return None
+        dfa (DFA): The DFA to convert.
         
-def generate_connected_automata():
-    automaton = nx.DiGraph()
+    Returns:
+        Automata: The converted automata.
+    """
+    transitions = np.zeros((len(dfa.states), len(dfa.states)))
+    final_states = dfa.final_states
+    for state in dfa.states:
+        for symbol in dfa.alphabet:
+            transitions[state.name, dfa.transitions[state, symbol]].append(symbol.name)
+        
+    return Automata(transitions, final_states)
+    
+def make_automata_connected(dfa: DFA) -> DFA:
+    for state in dfa.states:
+        is_hole = is_hole_state(dfa, state)
+        if is_hole:
+            prob = 0.5
+            if np.random.rand() < prob:
+                state.is_final = True
+            else:
+                # make random transition from hole state
+                random_symbol = np.random.choice(dfa.alphabet)
+                dfa.transitions[random_symbol, state] = np.random.choice(dfa.alphabet)
 
-    # Definir los estados del autómata
-    states = ["q0", "q1", "q2", "q3"]
+def is_hole_state(dfa: DFA, state: State) -> bool:
+    for symbol in dfa.alphabet:
+        if dfa.transitions[state, symbol] != state:
+            return False
+    
+    return True
 
-    # Agregar estados como nodos al grafo
-    automaton.add_nodes_from(states)
+def make_automata_unique_accepting(dfa: DFA) -> DFA:
+    for state in dfa.states:
+        if dfa.is_final_state(state):
+            state.is_final = False
+            
+    # make random state final
+    dfa_size = len(dfa.states)
+    random_state = np.random.randint(dfa_size)
+    dfa.states[random_state].is_final = True
+    
+    return dfa
 
-    # Definir el estado inicial
-    initial_state = "q0"
-
-    # Definir los estados finales
-    final_states = ["q3"]
-
-    # Agregar arcos (transiciones) entre estados
-    automaton.add_edge("q0", "q1", label="a")
-    automaton.add_edge("q1", "q2", label="b")
-    automaton.add_edge("q2", "q3", label="c")
-
-    # Verificar la conectividad del autómata
-    if nx.is_strongly_connected(automaton):
-        print("El autómata es conexo.")
-    else:
-        print("El autómata no es conexo.")
-
-    # Para visualizar el autómata (requiere Matplotlib)
-    import matplotlib.pyplot as plt
-    nx.draw(automaton, with_labels=True, node_size=500, node_color="lightblue")
-    plt.show()
-
-
-def generate_fully_accepting_automata():
-    return NotImplementedError()
-
-def generate_minimal_automata():
-    return NotImplementedError()
+def make_automata_minimal(dfa: DFA) -> DFA:
+    learner = GeneralLearner(dfa)
+    minimal_dfa = learner.learn()
+    return minimal_dfa
