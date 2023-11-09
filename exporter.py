2ea6ad178adce187ed7f2156b4d074b645a1d0ce
AlejoGarat <alejogaratd@gmail.com>
2023-11-09 20:17:12 -0300

    BFS ordering

--- exporter.py
+++ exporter.py
@@ -1,5 +1,6 @@
 from automata import Automata
 import pickle 
+import numpy as np
 
 def export_automatas(automatas: [Automata], path: str):
     """Exports the automatas to a pickle file.
@@ -8,12 +9,50 @@ def export_automatas(automatas: [Automata], path: str):
         automatas ([Automata]): The automatas to export.
     """
     pkl_file = open(f'{path}.pkl', 'wb')
-    transitions_final_states = [(automata.transitions, automata.final_states, automata.alphabet, automata.initial_state,
-                                 automata.pos_dict) 
-                                for automata in automatas]
-    pickle.dump(transitions_final_states, pkl_file)
+    automata_info = bfs_processed_automata_info(automatas)
+    pickle.dump(automata_info, pkl_file)
     pkl_file.close()
-
+    
+def bfs_processed_automata_info(automatas: [Automata]) -> dict():
+    bfs_automatas_info = []
+    
+    for automata in automatas:
+        queue = []
+        node_dict = {}
+        initial_node = automata.pos_dict[automata.initial_state]
+        
+        order = -1
+        queue.append(initial_node)
+                
+        while len(queue) > 0:
+            order += 1
+            node = queue.pop(0)
+            node_dict[node] = order
+            for neighbor, connected_set in enumerate(automata.transitions[node]):
+                if connected_set and neighbor not in node_dict and neighbor not in queue:
+                    queue.append(neighbor)
+                    
+        new_transitions = np.zeros((len(automata.transitions), len(automata.transitions)), dtype=object)
+        for row in range(len(automata.transitions)):
+            for col in range(len(automata.transitions)):
+                new_row = node_dict[row]
+                new_col = node_dict[col]
+                
+                if automata.transitions[new_row, new_col]:
+                    new_transitions[row, col] = 1
+                else:
+                    new_transitions[row, col] = 0 
+                
+        new_final_states = set()
+        for state in automata.final_states:
+            new_final_states.add(node_dict[automata.pos_dict[state]])
+            
+        automata_info = {"transitions": new_transitions, "final_states": new_final_states, 
+                      "initial_state": automata.pos_dict[automata.initial_state]}
+        bfs_automatas_info.append(automata_info)
+        
+    return bfs_automatas_info
+                
 
 def read_automatas(path: str) -> [Automata]:
     """Reads the automatas from a pickle file.
